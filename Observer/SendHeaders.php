40b3cfc23d8561950325548cf1d68eda7ac085e2
neymir <anthony@bird.eu>
2022-03-19 17:20:01 +0100

    handling request header

--- Observer/SendHeaders.php
+++ Observer/SendHeaders.php
@@ -3,6 +3,7 @@ declare(strict_types = 1);
 
 namespace Opengento\EarlyHints\Observer;
 
+use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\App\Response\HeaderManager;
 use Magento\Framework\App\Response\Http;
 use Magento\Framework\Event\Observer;
@@ -21,15 +22,23 @@ class SendHeaders implements ObserverInterface
     protected HeaderManager $headerManager;
 
     /**
-     * @param Http          $httpResponse
-     * @param HeaderManager $headerManager
+     * @var ScopeConfigInterface
+     */
+    protected ScopeConfigInterface $scopeConfig;
+
+    /**
+     * @param Http                 $httpResponse
+     * @param HeaderManager        $headerManager
+     * @param ScopeConfigInterface $scopeConfig
      */
     public function __construct(
         Http $httpResponse,
-        HeaderManager $headerManager
+        HeaderManager $headerManager,
+        ScopeConfigInterface $scopeConfig
     ) {
         $this->httpResponse  = $httpResponse;
         $this->headerManager = $headerManager;
+        $this->scopeConfig   = $scopeConfig;
     }
 
     /**
@@ -39,31 +48,83 @@ class SendHeaders implements ObserverInterface
      */
     public function execute(Observer $observer)
     {
-        //@todo : possibilité de se mettre encore plus haut si on maitrise les headers envoyés !!!
         $httpResponse = $this->httpResponse;
 
-        //Add 103 headers + link (based on config / cache, or anything)
-        //@todo: dynamic links
-        $httpResponse->setHeader('Early-Hints', 'true');
-        $httpResponse->setHeader('Link', '</script.js>; rel=preload; as=script');
+        //Set default header to disable buffering
+        $this->setDefaultHeaderToDisableBuffering($httpResponse);
 
-        $httpResponse->setHeader('X-Accel-Buffering', 'no'); // for nginx
-        $httpResponse->setHeader('Surrogate-Control', 'OpengentoBroadcast/1.0'); // for varnish
+        //Add preload link headers
+        $this->addPreloadLinkHeaders($httpResponse);
 
         //Calculate httpResponse headers based on HeaderProviders
         $this->headerManager->beforeSendResponse($httpResponse);
 
-        //$headers = $httpResponse->getHeaders();
-
         //@todo: add headers coming from CSP module (or other module with same architecture)
 
+        //Send all headers to browser
+        $this->sendAllHeadersToBrowser($httpResponse);
+    }
 
-        //@todo: sendHeaders and fake body
+    /**
+     * @param $type
+     *
+     * @return string[]
+     */
+    protected function _getStaticUris($type): array
+    {
+        $value = $this->scopeConfig->getValue('opengento/earlyhint/uri_' . $type);
+
+        return \explode("\n", $value);
+    }
+
+    /**
+     * @param Http  $httpResponse
+     *
+     * @return void
+     */
+    protected function addPreloadLinkHeaders(Http $httpResponse): void
+    {
+        $stylesUri    = $this->_getStaticUris('style');
+        $scriptsUri   = $this->_getStaticUris('script');
+
+        $linkHeader = [];
+        foreach ($stylesUri as $uri) {
+            $linkHeader[] = "<{$uri}>; rel=preload; as=style";
+        }
+
+        //Loop scripts
+        foreach ($scriptsUri as $uri) {
+            $linkHeader[] = "<{$uri}>; rel=preload; as=script";
+        }
+
+        $httpResponse->setHeader('Link', \implode(',', $linkHeader));
+    }
+
+    /**
+     * @param Http $httpResponse
+     *
+     * @return void
+     */
+    protected function setDefaultHeaderToDisableBuffering(Http $httpResponse): void
+    {
+        $httpResponse->setHeader('Early-Hints', 'true');
+        $httpResponse->setHeader('X-Accel-Buffering', 'no'); // for nginx no buffering
+        $httpResponse->setHeader(
+            'Surrogate-Control',
+            'OpengentoBroadcast/1.0'); // for varnish no buffering (vcl have to be modified, check Readme.md)
+    }
+
+    /**
+     * @param Http $httpResponse
+     *
+     * @return void
+     */
+    protected function sendAllHeadersToBrowser(Http $httpResponse): void
+    {
         $httpResponse->sendHeaders();
-        echo '...fake content to trigger headers to be sent to browser';
+
+        //@todo : find a better way to trigger header to be sent to browser
+        echo '<div></div>';
         flush();
-        sleep(10); //then all the rest of magento code will be calculated (block, layout, ...)
-        //@todo: cuttoff default sendHeaders on beforeSendResponse to avoid "headers already sent" error
-        ///home/web/opengento/vendor/laminas/laminas-http/src/PhpEnvironment/Response.php::send() -> keep only sendContent()
     }
 }
